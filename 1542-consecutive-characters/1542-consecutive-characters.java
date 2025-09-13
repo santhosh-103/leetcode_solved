c7c9d3fcb4413a86eab4a5489152d9fc46b8bde0
Santhosh <santhosh00cse@gmail.com>
2025-09-13 20:01:54 +0530

    Time: 1 ms (99.59%), Space: 42.5 MB (13.24%) - LeetHub

---
+++ 1542-consecutive-characters/1542-consecutive-characters.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxPower(String s) {
+       char[] c=s.toCharArray();
+       int max=1;
+       int count=1;
+       for(int i=1;i<c.length;i++){
+        
+          char ch=c[i];
+          if(c[i]==c[i-1]) count++;
+          else count=1;
+          max=Math.max(count,max);
+       } 
+       return max;
+    }
+}
