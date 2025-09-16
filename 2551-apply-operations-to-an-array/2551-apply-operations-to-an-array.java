9515a1c6aa44ad6b80f5df6456e1217bf8049f9c
Santhosh <santhosh00cse@gmail.com>
2025-09-16 18:55:29 +0530

    Time: 1 ms (93.12%), Space: 43.7 MB (75.02%) - LeetHub

--- 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.java
+++ 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.java
@@ -1,5 +1,20 @@
 class Solution {
     public int[] applyOperations(int[] nums) {
-        
+        for(int i=0;i<nums.length-1;i++){
+            if(nums[i]==nums[i+1]){
+                nums[i]=nums[i]*2;
+                nums[i+1]=0;
+            }
+        }
+        int index=0;
+        for(int i:nums){
+            if(i!=0){
+                nums[index++]=i;
+            }
+        }
+        while(index<nums.length){
+            nums[index++]=0;
+        }
+        return nums;
     }
 }
