91534df03bb96f0ed1924ee3c38db20962e66987
Santhosh <santhosh00cse@gmail.com>
2025-10-01 22:36:10 +0530

    Time: 3 ms (39.04%), Space: 43.3 MB (7.93%) - LeetHub

--- 1264-maximum-number-of-words-you-can-type/1264-maximum-number-of-words-you-can-type.java
+++ 1264-maximum-number-of-words-you-can-type/1264-maximum-number-of-words-you-can-type.java
@@ -4,15 +4,15 @@ class Solution {
         char[] c=brokenLetters.toCharArray();
         int ans=0;
         for(int i=0;i<arr.length;i++){
-            int count=0;
+            boolean b=true;
             for(int j=0;j<c.length;j++){
                 char ch=c[j];
                 if(arr[i].contains(String.valueOf(ch))) {
-                    count+=1;
+                    b=false;
                     break;
                 }
             }
-            if(count!=1) ans+=1;
+            if(b) ans+=1;
         }
         return ans;
     }
