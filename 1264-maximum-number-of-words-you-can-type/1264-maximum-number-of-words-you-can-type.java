041085bd82a04efdb557a9f2e38b9ef63ad24f8e
Santhosh <santhosh00cse@gmail.com>
2025-10-01 22:32:44 +0530

    Time: 6 ms (13.7%), Space: 44.7 MB (5.66%) - LeetHub

---
+++ 1264-maximum-number-of-words-you-can-type/1264-maximum-number-of-words-you-can-type.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int canBeTypedWords(String text, String brokenLetters) {
+        String[] arr=text.split(" ");
+        char[] c=brokenLetters.toCharArray();
+        int ans=0;
+        for(int i=0;i<arr.length;i++){
+            int count=0;
+            for(int j=0;j<c.length;j++){
+                char ch=c[j];
+                if(arr[i].contains(String.valueOf(ch))) count++;
+            }
+            if(count==0) ans+=1;
+        }
+        return ans;
+    }
+}
